4d5f08f0d7007ead2f7fa227a83f4b9a3af5d645
gera260404 <115124507+gera260404@users.noreply.github.com>
2025-11-13 10:12:42 -0600

    BD-aw
    Subo respaldo inicial base de datos

---
+++ aw.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 13-11-2025 a las 16:10:25
+-- Versión del servidor: 5.7.17-log
+-- Versión de PHP: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `aw`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+CREATE TABLE `alumnos` (
+  `codigo` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `email` varchar(70) NOT NULL,
+  `codigocurso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `alumnos`
+--
+
+INSERT INTO `alumnos` (`codigo`, `nombre`, `email`, `codigocurso`) VALUES
+(11, 'gera', 'gera123@gmail.com', 2),
+(103, 'gera', 'at@gmail.com', 1),
+(104, 'aaaaa', 'at@gmail.com', 1),
+(105, 'xd', 'at@gmail.com', 1),
+(106, 'aaaaaaaa', 'at@gmail.com', 1),
+(107, 'aw', 'at@gmail.com', 1),
+(108, 'qw', 'at@gmail.com', 1),
+(109, 'zx', 'at@gmail.com', 1),
+(110, 'e', 'at@gmail.com', 1),
+(143, 'ar', 'ar@gmail.com', 2),
+(145, 'estrella', 'estrella@gmail.com', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=146;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
